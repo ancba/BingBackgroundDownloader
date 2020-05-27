ed7c2f24dfe5565c8ab703576c4e3bc0a5fa28b1
ancba <2176678607@qq.com>
2020-05-27 14:01:42 +0800

    添加了新的main.py文件，用于启动程序 原先的main.py更名为git_image.py
    在get_image.py中
    删除了部分不必要的提示
    添加了新函数start以启动函数

---
+++ get_image.py
@@ -0,0 +1,26 @@
+import time
+import urllib.request as request
+
+def get_link ():
+	bing = request.urlopen('https://www.bing.com')
+	html = str(bing.read())
+	postion1 = html.find('href="/')
+	postion2 = html.find('" as="image"') #获取壁纸链接
+        global link
+	link = 'https://www.bing.com' + html[postion1 + 6:postion2]
+
+def download (link):
+	bg = request.urlopen(link)
+	image = bg.read()
+	f = open('Bingimage' + str(time.localtime()[0]) + str(time.localtime()[1]) + str(time.localtime()[2]) + '.jpg','wb')
+	f.write(image)
+        f.close()
+
+def start ():
+    try:
+        get_link()
+        download(link)
+    except:
+        OSError as error:
+            print(error)
+            get_link()
--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-import time
-import urllib.request as request
+import get_image
 
-def get_link ():
-	bing = request.urlopen('https://www.bing.com')
-	html = str(bing.read())
-	postion1 = html.find('href="/')
-	postion2 = html.find('" as="image"')
-	link = 'https://www.bing.com' + html[postion1 + 6:postion2]
-	print ('已获取到背景链接地址' + link)
-	download(link)
-
-def download (link):
-	print('开始下载')
-	bg = request.urlopen(link)
-	write = bg.read()
-	f = open('Bingimage' + str(time.localtime()[0]) + str(time.localtime()[1]) + str(time.localtime()[2]) + '.jpg','wb')
-	f.write(write)
-	print('下载已完毕')
-
-if __name__ == '__main__':
-	get_link()
+if __main__ == '__name__':
+    get_image.start()
