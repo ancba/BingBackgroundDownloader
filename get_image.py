a21f9c329a2de75e64c9e05fcc6f7c3cc364b1d9
ancba <2176678607@qq.com>
2020-05-28 12:51:48 +0800

    修复bug

--- get_image.py
+++ get_image.py
@@ -2,22 +2,24 @@ import time
 import urllib.request as request
 
 def get_link ():
-	bing = request.urlopen('https://www.bing.com')
-	html = str(bing.read())
-	postion1 = html.find('href="/')
-	postion2 = html.find('" as="image"') #获取壁纸链接
-	link = 'https://www.bing.com' + html[postion1 + 6:postion2]
+    bing = request.urlopen('https://www.bing.com')
+    html = str(bing.read())
+    postion1 = html.find('href="/')
+    postion2 = html.find('" as="image"') #获取壁纸链接
+    global link
+    link = 'https://www.bing.com' + html[postion1 + 6:postion2]
 
-def download (link):
-	bg = request.urlopen(link)
-	image = bg.read()
-	f = open('Bingimage' + str(time.localtime()[0]) + str(time.localtime()[1]) + str(time.localtime()[2]) + '.jpg','wb')
-	f.write(image)
+def download ():
+    bg = request.urlopen(link)
+    image = bg.read()
+    f = open('Bingimage' + str(time.localtime()[0]) + str(time.localtime()[1]) + str(time.localtime()[2]) + '.jpg','wb')
+    f.write(image)
+    f.close()
 
 def start ():
     try:
         get_link()
-        download(link)
+        download()
     except OSError as error:
             print(error)
             get_link()
