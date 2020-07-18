0e59b9c67427dafd0fcc0c10fd2513f32ca1601d
ancba <2176678607@qq.com>
2020-07-18 17:38:48 +0800

    增强了代码的可读性

--- get_image.py
+++ get_image.py
@@ -5,27 +5,25 @@ def get_link ():
     bing = request.urlopen('https://www.bing.com')
     html = str(bing.read())
     postion1 = html.find('href="/')
-    postion2 = html.find('" as="image"') #获取壁纸链接
-    global link
-    link = 'https://www.bing.com' + html[postion1 + 6:postion2]
+    postion2 = html.find('" as="image"') #获取壁纸链接两端位置
+    link = 'https://www.bing.com' + html[postion1 + 6:postion2] #合成链接
+    return link
 
-def download (path):
+def download (path,link):
     bg = request.urlopen(link)
     image = bg.read()
     #判断路径
     if path == 0 or path == '0':
         f = open('Bingimage' + str(time.localtime()[0]) + str(time.localtime()[1]) + str(time.localtime()[2]) + '.jpg','wb')
-        f.write(image)
-        f.close()
     else:
         f = open(path + 'Bingimage' + str(time.localtime()[0]) + str(time.localtime()[1]) + str(time.localtime()[2]) + '.jpg','wb')
-        f.write(image)
-        f.close()
+    f.write(image) #写入
+    f.close()
 
 def start (path=0):
     try:
-        get_link()
-        download(path)
+        link = get_link()
+        download(path,link)
     except OSError as error:
-            print(error)
-            exit()
+         print(error)
+         exit()
